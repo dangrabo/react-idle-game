7179fa7c57f195011d6825508fe5b8b8c16ab56d
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-23 13:27:41 -0700

    Simple Change

--- src/components/Upgrade.jsx
+++ src/components/Upgrade.jsx
@@ -1,13 +1,13 @@
 export default function Upgrade({ upgrades, handleUpgrade, type }) {
-  
-  const upgradeCost = type === 'click' ? upgrades.clickUpgradeCost : upgrades.timeUpgradeCost;
-  
+  const upgradeCost =
+    type === "click" ? upgrades.clickUpgradeCost : upgrades.timeUpgradeCost;
+
   return (
     <div className="upgradeDiv">
-        <p>Upgrade Clicker</p>
-        <button onClick={handleUpgrade} id="upgrade-clicker">
-          Cost: {upgradeCost}
-        </button>
-      </div>
-  )
+      <p>Upgrade {type}</p>
+      <button onClick={handleUpgrade} id="upgrade-clicker">
+        Cost: {upgradeCost}
+      </button>
+    </div>
+  );
 }
